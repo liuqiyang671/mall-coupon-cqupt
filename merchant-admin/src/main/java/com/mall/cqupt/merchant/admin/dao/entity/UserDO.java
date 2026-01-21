e56e0fabb6c261bdb59fba6da1eb59004fbb623a
liuqiyang <yang10637@gmail.com>
2026-01-21 15:35:29 +0800

    feature: 创建用户数据Mock单元测试，为后续的优惠券业务分库分表做铺垫

---
+++ merchant-admin/src/main/java/com/mall/cqupt/merchant/admin/dao/entity/UserDO.java
@@ -0,0 +1,70 @@
+package com.mall.cqupt.merchant.admin.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * 商家后管用户数据库持久层实体｜当前仅用于 Mock 数据
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@TableName("t_user")
+public class UserDO {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 店铺编号
+     */
+    private String shopNumber;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 手机号
+     */
+    private String phone;
+
+    /**
+     * 邮箱
+     */
+    private String mail;
+
+    /**
+     * 创建时间
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    /**
+     * 修改时间
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
+
+    /**
+     * 删除标识 0：未删除 1：已删除
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private Integer delFlag;
+}
---
+++ merchant-admin/src/main/java/com/mall/cqupt/merchant/admin/dao/mapper/UserMapper.java
@@ -0,0 +1,10 @@
+package com.mall.cqupt.merchant.admin.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mall.cqupt.merchant.admin.dao.entity.UserDO;
+
+/**
+ * 商家后管用户数据库持久层｜当前仅用于 Mock 数据
+ */
+public interface UserMapper extends BaseMapper<UserDO> {
+}
