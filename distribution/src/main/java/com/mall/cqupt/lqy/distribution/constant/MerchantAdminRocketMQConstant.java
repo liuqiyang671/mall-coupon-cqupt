ecf1abebc5dbc5e53322abfaf43cb869007c68f5
liuqiyang <yang10637@gmail.com>
2026-01-26 14:56:57 +0800

    feature: 开发优惠券定时推送业务消费者，调用真实发送生产者

---
+++ distribution/src/main/java/com/mall/cqupt/lqy/distribution/constant/MerchantAdminRocketMQConstant.java
@@ -0,0 +1,17 @@
+package com.mall.cqupt.lqy.distribution.constant;
+
+/**
+ * 商家后管优惠券 RocketMQ 常量类
+ */
+public final class MerchantAdminRocketMQConstant {
+
+    /**
+     * 优惠券模板推送定时执行 Topic Key
+     */
+    public static final String TEMPLATE_TASK_DELAY_TOPIC_KEY = "one-coupon_merchant-admin-service_coupon-task-delay_topic${unique-name:}";
+
+    /**
+     * 优惠券模板推送定时执行-执行发送开始执行指令消费者组 Key
+     */
+    public static final String TEMPLATE_TASK_SEND_EXECUTE_CG_KEY = "one-coupon_merchant-admin-service_coupon-task-send-execute_cg${unique-name:}";
+}
---
+++ distribution/src/main/java/com/mall/cqupt/lqy/distribution/mq/consumer/CouponTaskSendExecuteConsumer.java
@@ -0,0 +1,41 @@
+package com.mall.cqupt.lqy.distribution.mq.consumer;
+
+import com.alibaba.fastjson2.JSON;
+
+import com.mall.cqupt.lqy.distribution.constant.MerchantAdminRocketMQConstant;
+import com.mall.cqupt.lqy.distribution.mq.base.MessageWrapper;
+import com.mall.cqupt.lqy.distribution.mq.event.CouponTaskDelayEvent;
+import com.mall.cqupt.lqy.distribution.mq.event.CouponTaskExecuteEvent;
+import com.mall.cqupt.lqy.distribution.mq.producer.CouponTaskActualExecuteProducer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.rocketmq.spring.annotation.RocketMQMessageListener;
+import org.apache.rocketmq.spring.core.RocketMQListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * 优惠券推送定时执行-发送执行命令消费者
+ */
+@Component
+@RequiredArgsConstructor
+@RocketMQMessageListener(
+        topic = MerchantAdminRocketMQConstant.TEMPLATE_TASK_DELAY_TOPIC_KEY,
+        consumerGroup = MerchantAdminRocketMQConstant.TEMPLATE_TASK_SEND_EXECUTE_CG_KEY
+)
+@Slf4j(topic = "CouponTaskSendExecuteConsumer")
+public class CouponTaskSendExecuteConsumer implements RocketMQListener<MessageWrapper<CouponTaskDelayEvent>> {
+
+    private final CouponTaskActualExecuteProducer couponTaskActualExecuteProducer;
+
+    @Override
+    public void onMessage(MessageWrapper<CouponTaskDelayEvent> messageWrapper) {
+        // 开头打印日志，平常可 Debug 看任务参数，线上可报平安（比如消息是否消费，重新投递时获取参数等）
+        log.info("[消费者] 优惠券推送定时执行@发送执行命令 - 执行消费逻辑，消息体：{}", JSON.toJSONString(messageWrapper));
+
+        // 执行优惠券推送业务，正式向用户发放优惠券
+        CouponTaskExecuteEvent couponTaskExecuteEvent = CouponTaskExecuteEvent.builder()
+                .couponTaskId(messageWrapper.getMessage().getCouponTaskId())
+                .build();
+        couponTaskActualExecuteProducer.sendMessage(couponTaskExecuteEvent);
+    }
+}
