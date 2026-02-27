6138817a98948ba0c8d0c549933403eaa11c7420
liuqiyang <yang10637@gmail.com>
2026-02-27 19:05:56 +0800

    feature: 开发用户优惠券持久层功能

---
+++ distribution/src/main/java/com/mall/cqupt/lqy/distribution/dao/entity/UserCouponDO.java
@@ -0,0 +1,100 @@
+package com.mall.cqupt.lqy.distribution.dao.entity;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * 用户优惠券数据库持久层实体
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@TableName("t_user_coupon")
+public class UserCouponDO {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    /**
+     * 用户id
+     */
+    private Long userId;
+
+    /**
+     * 订单号
+     */
+    private String orderSn;
+
+    /**
+     * 优惠券模板id
+     */
+    private Long couponTemplateId;
+
+    /**
+     * 领取时间
+     */
+    private Date receiveTime;
+
+    /**
+     * 领取次数
+     */
+    private Integer receiveCount;
+
+    /**
+     * 有效期开始时间
+     */
+    private Date validStartTime;
+
+    /**
+     * 有效期结束时间
+     */
+    private Date validEndTime;
+
+    /**
+     * 使用时间
+     */
+    private Date useTime;
+
+    /**
+     * 点击跳转地址
+     */
+    private String redirectUrl;
+
+    /**
+     * 券来源 0：领券中心 1：平台发放 2：店铺领取
+     */
+    private Integer source;
+
+    /**
+     * 状态 0：未使用 1：已使用 2：已过期 3：已撤回
+     */
+    private Integer status;
+
+    /**
+     * 创建时间
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private Date createTime;
+
+    /**
+     * 修改时间
+     */
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updateTime;
+
+    /**
+     * 删除标识 0：未删除 1：已删除
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private Integer delFlag;
+}
---
+++ distribution/src/main/java/com/mall/cqupt/lqy/distribution/dao/mapper/UserCouponMapper.java
@@ -0,0 +1,10 @@
+package com.mall.cqupt.lqy.distribution.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mall.cqupt.lqy.distribution.dao.entity.UserCouponDO;
+
+/**
+ * 用户优惠券数据库持久层
+ */
+public interface UserCouponMapper extends BaseMapper<UserCouponDO> {
+}
