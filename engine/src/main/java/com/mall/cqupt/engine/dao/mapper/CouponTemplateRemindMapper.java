448e23e97970f2ffde5c75f310989fed33dd8a9b
liuqiyang <yang10637@gmail.com>
2026-03-03 14:14:00 +0800

    feature: 新增优惠券预约提醒DO数据对象及持久层

---
+++ engine/src/main/java/com/mall/cqupt/engine/dao/mapper/CouponTemplateRemindMapper.java
@@ -0,0 +1,10 @@
+package com.mall.cqupt.engine.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.mall.cqupt.engine.dao.entity.CouponTemplateRemindDO;
+
+/**
+ * 用户预约提醒信息存储数据库持久层
+ */
+public interface CouponTemplateRemindMapper extends BaseMapper<CouponTemplateRemindDO> {
+}
