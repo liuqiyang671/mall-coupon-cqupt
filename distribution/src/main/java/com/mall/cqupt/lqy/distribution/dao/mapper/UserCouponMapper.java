ca020960e6c25505a84b8d015e78421f8334f23d
liuqiyang <yang10637@gmail.com>
2026-03-03 13:54:31 +0800

    feature: 开发查询用户优惠券最大领取次数，因为功能重构暂时用不到该方法

--- distribution/src/main/java/com/mall/cqupt/lqy/distribution/dao/mapper/UserCouponMapper.java
+++ distribution/src/main/java/com/mall/cqupt/lqy/distribution/dao/mapper/UserCouponMapper.java
@@ -2,9 +2,22 @@ package com.mall.cqupt.lqy.distribution.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.mall.cqupt.lqy.distribution.dao.entity.UserCouponDO;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
 
 /**
  * 用户优惠券数据库持久层
  */
 public interface UserCouponMapper extends BaseMapper<UserCouponDO> {
+    /**
+     * 查询用户优惠券最大领取次数
+     * 设计用户优惠券发放时考虑使用该方法，后来经过重构舍弃了该流程，等待后续看是否有机会重启
+     *
+     * @param couponTemplateId 优惠券模板 ID
+     * @param userIds          用户 ID 集合
+     * @return 用户优惠券最大领取次数集合
+     */
+    @Deprecated
+    List<UserCouponDO> selectUserCourseMaxReceiveCount(@Param("couponTemplateId") Long couponTemplateId, @Param("userIds") List<Long> userIds);
 }
