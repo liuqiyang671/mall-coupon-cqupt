b0ecbe86addff55e67be10f746b558f0982baad8
liuqiyang <yang10637@gmail.com>
2026-03-16 12:52:25 +0800

    feature: 添加防止消息队列消费者重复消息SpringBean

--- framework/src/main/java/com/mall/cqupt/framework/config/IdempotentConfiguration.java
+++ framework/src/main/java/com/mall/cqupt/framework/config/IdempotentConfiguration.java
@@ -1,14 +1,16 @@
 package com.mall.cqupt.framework.config;
 
 
+import com.mall.cqupt.framework.idempotent.NoMQRepeatConsumeAspect;
 import com.mall.cqupt.framework.idempotent.NoRepeatSubmitAspect;
 import org.redisson.api.RedissonClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 /**
  * 防重复提交组件配置类
  */
-public class NoRepeatSubmitConfiguration {
+public class IdempotentConfiguration {
 
     /**
      * 防止用户重复提交表单信息切面控制器
@@ -16,5 +18,13 @@ public class NoRepeatSubmitConfiguration {
     @Bean
     public NoRepeatSubmitAspect noRepeatSubmitAspect(RedissonClient redissonClient) {
         return new NoRepeatSubmitAspect(redissonClient);
+    }
+
+    /**
+     * 防止消息队列消费者重复消费消息切面控制器
+     */
+    @Bean
+    public NoMQRepeatConsumeAspect noMQRepeatConsumeAspect(StringRedisTemplate stringRedisTemplate) {
+        return new NoMQRepeatConsumeAspect(stringRedisTemplate);
     }
 }
